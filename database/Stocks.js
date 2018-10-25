bd880e61c99068b7acdf3192218f3ab4dbd51ed5
Ben-Mustion <benmustion@gmail.com>
2018-10-24 19:18:22 -0700

    Database schema started

---
+++ database/Stocks.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const db = require('./index.js');
+mongoose.Promise = global.Promise;
+
+const stockDataSchema = new mongoose.Schema({
+  name: String,
+  rating: Number,
+  ratingBlurb: String,
+  price: Number,
+  priceChange: Number
+}, 
+  {
+    timestamps: true
+  }
+);
+
+const Stocks = mongoose.model('Stocks', stockDataSchema);
+
+module.exports = Stocks;
---
+++ database/index.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+const mongoUri = 'mongodb://localhost/RobinhoodFEC';
+const db = mongoose.connect(mongoUri);
+
+module.exports = db;
