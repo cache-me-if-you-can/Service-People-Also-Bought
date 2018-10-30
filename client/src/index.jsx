d1d79bbabebc120c9a739d3bce07215124f87815
Ben-Mustion <benmustion@gmail.com>
2018-10-29 19:26:10 -0700

    Set up server and react app

---
+++ client/src/index.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
+
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      stocks: [{
+        name: '',
+        rating: 0,
+        ratingBlurb: '',
+        price: 0,
+        priceChange: 0,
+      }],
+    }
+    this.handleGet.bind(this);
+  }
+  componentDidMount() {
+    this.handleGet();  
+  }
+
+  handleGet() {
+    $.get('http://localhost:3007/api', (data) => console.log(data)
+    )
+  }
+
+  render() {
+    const name = this.state.stocks[0].name;
+    const rating = this.state.stocks[0].rating;
+    const ratingBlurb = this.state.stocks[0].ratingBlurb;
+    const price = this.state.stocks[0].price;
+    const priceChange = this.state.stocks[0].priceChange;
+
+    return (
+      <div>
+        <div>Hello From React</div>
+        <div className='name'>{name}</div>
+        <div className='rating'>{rating}</div>
+        <div className='ratingBlurb'>{ratingBlurb}</div>
+        <div className='price'>{price}</div>
+        <div className='priceChange'>{priceChange}</div>
+      </div>
+    )
+  }
+
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-// const Stocks = require('../database/Stocks');
+const Stocks = require('../database/Stocks.js');
 // const db = require('../database/index');
 
 const app = express();
@@ -11,6 +11,21 @@ const PORT = 3007;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '/../client/dist')));
+
+app.get('/api', (req, res) => {
+  // Stocks.aggregate([{ $sample: {size: 9}}]).then(function (res) {
+  //   console.log(res);
+  // })
+  console.log('hi')
+  Stocks.find({}, (error, results) => {
+    if (error) {
+      res.status(500).send(error);
+    } else {
+      console.log('ajoiwejfioawoijf');
+      res.status(200).send(results);
+    }
+  })
+})
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
