12faa7a617632d82ec8fb2aff2cfd7f37ad5c7ee
Ben-Mustion <benmustion@gmail.com>
2018-10-30 15:56:55 -0700

    Commit merge changes

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -18,16 +18,7 @@ class App extends React.Component {
   }
 
   render() {
-<<<<<<< HEAD
     console.log(this.state)
-=======
-    const name = this.state.name;
-    const rating = this.state.stocks[0].rating;
-    const ratingBlurb = this.state.stocks[0].ratingBlurb;
-    const price = this.state.stocks[0].price;
-    const priceChange = this.state.stocks[0].priceChange;
-
->>>>>>> 0c6cd2495de0d167c0883630cebc32ced978851d
     return (
       <div>
         <div>Hello From React</div>
