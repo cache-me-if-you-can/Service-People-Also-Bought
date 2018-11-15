6e6b3ef48cb6630f8e17a15926fef1e09559bd15
Emely <mly.molina.minaya@gmail.com>
2018-11-14 17:52:47 -0800

    CRUD API and Readme file updeted

--- database/index.js
+++ database/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const mongoUri = 'mongodb://Ben:Benm1024@ds157223.mlab.com:57223/robinhoodfec';
+const mongoUri = 'mongodb://localhost/robinhoodfec';
 const db = mongoose.connect(mongoUri);
 
 module.exports = db;
+
