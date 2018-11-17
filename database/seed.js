7270ab73fb6edb549d4c9e26fbbf73d03251ed15
Emely <mly.molina.minaya@gmail.com>
2018-11-17 15:54:46 -0800

    data generation update

--- database/seed.js
+++ database/seed.js
@@ -1,6 +1,5 @@
 const faker = require('faker');
 const fs = require('fs');
-const csvWriter = require('csv-write-stream');
 // const db = require('./index.js');
 // const Stocks = require('./Stocks.js');
 const generateRandomStockPrice = () => (Math.random() * 999).toFixed(2);
@@ -15,28 +14,48 @@ const buyOrSell = () => {
   return arr[Math.round(Math.random())];
 };
 
-const sampleGenerator = () => {
-  const stocks = [];
-  const writer = csvWriter({ headers: ['id', 'name', 'rating', 'ratingBlurb', 'price', 'priceChange'] });
-  writer.pipe(fs.createWriteStream('out.csv'));
-  for (let i = 1; i <= 5e6; i += 1) {
-    const obj = {};
-    obj.id = i;
-    obj.name = faker.company.companyName();
-    obj.rating = generateRandomRating();
-    obj.ratingBlurb = `${obj.rating} % of analysts agree this stock is a ${buyOrSell()}`;
-    obj.price = generateRandomStockPrice();
-    obj.priceChange = generateRandomPriceChange();
-    writer.write([obj.id, obj.name, obj.rating, obj.ratingBlurb, obj.price, obj.priceChange]);
-    if (i === 1e6 || i === 2e6 || i === 3e6 || i === 4e6 || i === 5e6
-      || i === 6e6 || i === 7e6 || i === 8e6 || i === 9e6 || i === 10e6) {
-      console.log(i);
-    }
+const sampleGenerator = (index) => {
+  const obj = {};
+  obj.id = index;
+  obj.name = faker.company.companyName();
+  obj.rating = generateRandomRating();
+  obj.ratingBlurb = `${obj.rating} % of analysts agree this stock is a ${buyOrSell()}`;
+  obj.price = generateRandomStockPrice();
+  obj.priceChange = generateRandomPriceChange();
+  const record = `${obj.id}, ${obj.name}, ${obj.rating}, ${obj.ratingBlurb}, ${obj.price}, ${obj.priceChange}\n`;
+  if (index === 1000000 || index === 2000000 || index === 3000000 || index === 4000000 || index === 5000000
+    || index === 6000000 || index === 7000000 || index === 8000000 || index === 9000000 || index === 10000000) {
+    console.log(index);
   }
-  writer.end();
+  return record;
 };
 
-sampleGenerator();
+const streamStocks = fs.createWriteStream('stocks.csv');
+
+function writeStocks(writer) {
+  let i = 10e6;
+  let index = 1;
+  write();
+  function write() {
+    let ok = true;
+    do {
+      i -= 1;
+      index += 1;
+      if (i === 0) {
+        writer.write(sampleGenerator(index));
+      } else {
+        ok = writer.write(sampleGenerator(index));
+      }
+    } while (i > 0 && ok);
+    if (i > 0) {
+      writer.once('drain', write);
+    }
+  }
+}
+
+
+writeStocks(streamStocks);
+// csvWriter.end();
 
 // const insertSampleStocks = () => {
 //   Stocks.create(sampleGenerator())
