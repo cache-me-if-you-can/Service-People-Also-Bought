96297b7905a3780c74d762603f8f32c7e05f427d
Emely <mly.molina.minaya@gmail.com>
2018-12-02 21:18:54 -0800

    psql query improvement

--- server/server_psql.js
+++ server/server_psql.js
@@ -26,27 +26,21 @@ const client = new Client({
 client.connect();
 
 app.get('/api/alsobought/:id', (req, res) => {
-  client.query(`SELECT * from transactions WHERE stock_id = ${req.params.id}`, (firstError, result) => {
-    if (firstError) { res.status(500).send(firstError); } else {
-      const users = [];
-      result.rows.map((transaction) => {
-        users.push(transaction.user_buy_id);
-      });
-      client.query(`SELECT stock_id FROM transactions WHERE user_buy_id IN (${users.join(', ')}) LIMIT 20`, (secondError, stocksResult) => {
-        if (secondError) { res.status(500).send(secondError); } else {
-          const stocks = [];
-          stocksResult.rows.map((stock) => {
-            if (!stocks.includes(stock.stock_id) && stocks.length < 12) {
-              stocks.push(stock.stock_id);
-            }
-          });
-          client.query(`SELECT * FROM stocks WHERE id IN (${stocks.join(', ')})`, (thirdError, relatedStocks) => {
-            if (thirdError) { res.status(500).send(thirdError); } else {
-              res.status(200).send(relatedStocks.rows);
-            }
-          });
-        }
-      });
+  const stockQuery = `SELECT * 
+  FROM stocks
+  WHERE id IN (
+    SELECT DISTINCT t.stock_id
+    FROM transactions t
+    WHERE user_buy_id IN (
+      SELECT user_buy_id 
+      FROM transactions 
+      WHERE stock_id = ${req.params.id}
+    ) LIMIT 12
+  )`;
+
+  client.query(stockQuery, (thirdError, relatedStocks) => {
+    if (thirdError) { res.status(500).send(thirdError); } else {
+      res.status(200).send(relatedStocks.rows);
     }
   });
 });
